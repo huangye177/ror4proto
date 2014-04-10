78b12c69fd773b92b596d3f7dfccc83a811ab493
ye huang <huangye177@gmail.com>
2014-04-10 21:03:22 +0200

    playing with depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ror4proto::Application.config.secret_key_base = '339217d4c62eb4f5305786f01651cc37bb71a7786b2e5027a4c4b94d3974cd82ec8dbe13d19b1370dd7dce289552db7eb601ae1da8aa42361ef6a1640362d69b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ror4proto::Application.config.session_store :cookie_store, key: '_ror4proto_session'
